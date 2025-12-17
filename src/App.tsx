87eaef530a4a3923370a6e2138688644412ff21c
lxstik <egorrfal32@gmail.com>
2025-12-17 13:31:35 +0100

    contenedores principales

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,16 @@ console.log(menuItems)
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de propinas y Consumo</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
